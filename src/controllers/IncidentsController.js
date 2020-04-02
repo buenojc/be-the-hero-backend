86679df60d04e8dc4838cf511a51d2e8d8137e4c
Júlio César Bueno <buenojc@outlook.com>
2020-04-02 00:22:50 -0300

    Add the delete route to IncidentsController

--- src/controllers/IncidentsController.js
+++ src/controllers/IncidentsController.js
@@ -4,7 +4,7 @@ module.exports = {
   async index(req, res) {
     const incidents = await connection("incidents").select("*");
 
-    return res.json({ incidents });
+    return res.json(incidents);
   },
 
   async create(req, res) {
@@ -19,5 +19,29 @@ module.exports = {
     });
 
     return res.json({ id });
+  },
+
+  async delete(req, res) {
+    const { id } = req.params;
+    const ong_id = req.headers.authorization;
+
+    const incident = await connection("incidents")
+      .where("id", id)
+      .select("ong_id")
+      .first();
+
+    if (!incident) {
+      return res.status(404).json({ erro: "Incident not found" });
+    }
+
+    if (incident.ong_id !== ong_id) {
+      return res.status(401).json({ error: "Operation not permitted" });
+    }
+
+    await connection("incidents")
+      .where("id", id)
+      .delete();
+
+    return res.status(204).send();
   }
 };
