66855e38038a675f165dad6a4a9dd080f8c0f6d1
Júlio César Bueno <buenojc@outlook.com>
2020-04-03 22:58:48 -0300

    Back-end done
    Added pagination and informations of incidents + added cors

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const routes = express.Router();
+
 const OngController = require("./controllers/OngController");
 const IncidentsController = require("./controllers/IncidentsController");
 const ProfileController = require("./controllers/ProfileController");
-const routes = express.Router();
+const SessionController = require("./controllers/SessionController");
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
+
+routes.post("/sessions", SessionController.create);
 
 routes.get("/incidents", IncidentsController.index);
 routes.post("/incidents", IncidentsController.create);
