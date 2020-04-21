7bffb9f21f706a591c4b933730508f7df153d887
Júlio César Bueno <buenojc@outlook.com>
2020-04-21 01:32:29 -0300

    Create the route validations

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { celebrate, Joi, Segments } = require("celebrate");
 const routes = express.Router();
 
 const OngController = require("./controllers/OngController");
@@ -7,7 +8,19 @@ const ProfileController = require("./controllers/ProfileController");
 const SessionController = require("./controllers/SessionController");
 
 routes.get("/ongs", OngController.index);
-routes.post("/ongs", OngController.create);
+routes.post(
+  "/ongs",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      whatsapp: Joi.string().alphanum().required().min(10).max(11),
+      city: Joi.string().required(),
+      uf: Joi.string().length(2).required(),
+    }),
+  }),
+  OngController.create
+);
 
 routes.post("/sessions", SessionController.create);
 
@@ -15,6 +28,14 @@ routes.get("/incidents", IncidentsController.index);
 routes.post("/incidents", IncidentsController.create);
 routes.delete("/incidents/:id", IncidentsController.delete);
 
-routes.get("/profile", ProfileController.index);
+routes.get(
+  "/profile",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+  }),
+  ProfileController.index
+);
 
 module.exports = routes;
