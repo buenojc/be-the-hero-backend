03d91d1a713875152f0763430e566a7ddfe0ab9f
Júlio César Bueno <buenojc@outlook.com>
2020-04-23 01:53:04 -0300

    Separate the express config and the server call
    It was necessary because of the tests. To prevent the server be avaliable for users on testing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-const express = require("express");
-const routes = require("./routes");
-const { errors } = require("celebrate");
-const cors = require("cors");
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(routes);
-app.use(errors());
+const app = require('./app')
 
 app.listen(3333, () => {
-  console.log("Server is Running");
-});
+  console.log('Server is Running')
+})
