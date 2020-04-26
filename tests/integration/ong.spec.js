8d37372657b06c33fa24c2ca6a120966603531cf
Julio Cesar Bueno <buenojc@outlook.com>
2020-04-25 23:56:37 -0300

    Finish the Ong's integration test

--- tests/integration/ong.spec.js
+++ tests/integration/ong.spec.js
@@ -1,23 +1,27 @@
-const request = require('supertest')
-const app = require('../../src/app')
-const connection = require('../../src/database/connection')
+const request = require("supertest");
+const app = require("../../src/app");
+const connection = require("../../src/database/connection");
 
-describe('ONG', () => {
+describe("ONG", () => {
   beforeEach(async () => {
-    await connection.migrate.latest()
-  })
+    await connection.migrate.rollback();
+    await connection.migrate.latest();
+  });
 
-  it('should be able to create a new ong', async () => {
-    const response = await request(app)
-      .post('/ongs')
-      .send({
-        name: 'UIPA',
-        email: 'uipa@upa.com',
-        whatsapp: '11980967992',
-        city: 'Ribeirão Preto',
-        uf: 'SP'
-      })
+  afterAll(async () => {
+    await connection.destroy();
+  });
 
-    console.log(response.body)
-  })
-})
+  it("should be able to create a new ong", async () => {
+    const response = await request(app).post("/ongs").send({
+      name: "UIPA",
+      email: "uipa@upa.com",
+      whatsapp: "11980967992",
+      city: "Ribeirão Preto",
+      uf: "SP",
+    });
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.id).toHaveLength(8);
+  });
+});
